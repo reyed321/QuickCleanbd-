3da02b992d9b624de72d95d42032584698e5af68
shahid <shahidhasan66ss@gmail.com>
2023-07-09 15:16:11 +0600

    update commit

--- app/src/main/java/com/example/quickcleanbd/MainActivity.java
+++ app/src/main/java/com/example/quickcleanbd/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quickcleanbd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide the status bar
+
     }
 }
