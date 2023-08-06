d9ca4361ca095eba8bd48e589b11d28d468062a9
shahid <shahidhasan66ss@gmail.com>
2023-08-07 03:58:22 +0600

    update commit

---
+++ app/src/main/java/com/example/quickcleanbd/adapter/RecommendedAdapter.java
@@ -0,0 +1,75 @@
+package com.example.quickcleanbd.adapter;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quickcleanbd.Model.RecommendedModel;
+import com.example.quickcleanbd.R;
+
+import java.util.List;
+
+public class RecommendedAdapter extends RecyclerView.Adapter<RecommendedAdapter.ViewHolder> {
+
+    private Context context;
+    private List<RecommendedModel> itemList;
+
+    public RecommendedAdapter(List<RecommendedModel> itemList, Context context) {
+        this.itemList = itemList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecommendedAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recommended_item,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecommendedAdapter.ViewHolder holder, int position) {
+
+        RecommendedModel currentItem = itemList.get(position);
+
+        holder.laundryImage.setImageResource(currentItem.getImageResId());
+        holder.laundryName.setText(currentItem.getName());
+        holder.orderPrice.setText(currentItem.getPrice());
+        holder.laundryLocationIcon.setImageResource(R.drawable.ic_location); // Replace with your default icon resource
+        holder.distanceText.setText("Distance");
+        holder.distanceCount.setText(currentItem.getDistance());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView laundryImage;
+        TextView laundryName;
+        TextView orderPrice;
+        ImageView laundryLocationIcon;
+        TextView distanceText;
+        TextView distanceCount;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            laundryImage = itemView.findViewById(R.id.laundryImage);
+            laundryName = itemView.findViewById(R.id.laundryName);
+            orderPrice = itemView.findViewById(R.id.orderPrice);
+            laundryLocationIcon = itemView.findViewById(R.id.laundryLocationIcon);
+            distanceText = itemView.findViewById(R.id.distanceText);
+            distanceCount = itemView.findViewById(R.id.distanceCount);
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/quickcleanbd/adapter/RecyclerViewLaundry.java
+++
@@ -1,4 +0,0 @@
-package com.example.quickcleanbd.adapter;
-
-public class RecyclerViewLaundry {
-}
